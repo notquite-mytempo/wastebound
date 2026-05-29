a07cf3cbc3ded9ebab4ac1021bbacdc26ec7dbad
KOCTOLOM <kostiap19@gmail.com>
2026-05-29 10:22:08 +0300

    0.11 rocket base + main.js

--- scripts/Main.js
+++ scripts/Main.js
@@ -1,1 +1,6 @@
-//idfk
+importClass(java.lang.Class);
+const mod = Vars.mods.locateMod("wastebound");
+mod.meta.displayName = "[#6d9676]Wastebound[]";
+mod.meta.description = "[#585C4D]Welcome to Wastebound, a mod developed mostly alone that throws you into an irradiated named [#6d9676]Vastis[].\nTry and survive as you research new tech and destroy enemy bases.\n[#6B1E1E]THIS MOD IS STILL HEAVILY WORK IN PROGRESS[]";
+mod.meta.author = "Jonathan Not Quite My Tempo";
+mod.meta.subtitle = "Now with [yellow]85000% more radiation!";
